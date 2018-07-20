8893eeaf843eec0c20712621879e0120be1564a0
Elly Maimon <eliranmaimon@gmail.com>
2018-07-20 13:14:26 -0700

    add edit functionality to clients controller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -3,6 +3,7 @@ using HairSalon.Models;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Controllers
 {
@@ -58,6 +59,48 @@ namespace HairSalon.Controllers
         {
             Client client = db.Clients.FirstOrDefault(clients => clients.ClientId == id);
             db.Clients.Remove(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/clients/{id}/edit")]
+        public ActionResult Edit(int id)
+        {
+            Client client = db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+            ViewBag.StylistIds = db.Stylists.ToList()
+                .Select(stylist => new SelectListItem {
+                    Value = stylist.StylistId.ToString(),
+                    Text = stylist.StylistFirstName + " " + stylist.StylistLastName
+                })
+                .ToList();
+            return View(client);
+        }
+
+        [HttpPost("/clients/{id}/edit")]
+        public ActionResult Edit(Client client, List<int> StylistIds)
+        {
+            db.Entry(client).State = EntityState.Modified;
+            var clientMatchesInJoinTable = db.StylistClients.Where(entry => entry.ClientId == client.ClientId).ToList();
+
+            foreach (var stylist in clientMatchesInJoinTable)
+            {
+                int stylistId = stylist.StylistId;
+                var joinEntry = db.StylistClients
+                                    .Where(entry => entry.StylistId == stylistId)
+                                    .Where(entry => entry.ClientId == client.ClientId);
+                foreach (var entry in joinEntry)
+                {
+                    db.StylistClients.Remove(entry);
+                }
+            }
+
+            foreach (var id in StylistIds)
+            {
+                Stylist stylist = db.Stylists.FirstOrDefault(otherEntry => otherEntry.StylistId == id);
+                StylistClient newStylistClient = new StylistClient(client.ClientId, stylist.StylistId);
+                db.StylistClients.Add(newStylistClient);
+            }
+            
             db.SaveChanges();
             return RedirectToAction("Index");
         }
