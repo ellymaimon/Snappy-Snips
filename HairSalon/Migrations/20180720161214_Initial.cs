877b02fe96808c13886e57386e8f443a63fd0748
Elly Maimon <eliranmaimon@gmail.com>
2018-07-20 09:13:33 -0700

    successfully add initial migration and update to match models

---
+++ HairSalon/Migrations/20180720161214_Initial.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HairSalon.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ClientId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    ClientFirstName = table.Column<string>(nullable: true),
+                    ClientLastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specialties",
+                columns: table => new
+                {
+                    SpecialtyId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialties", x => x.SpecialtyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stylists",
+                columns: table => new
+                {
+                    StylistId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    StylistFirstName = table.Column<string>(nullable: true),
+                    StylistLastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stylists", x => x.StylistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StylistClients",
+                columns: table => new
+                {
+                    StylistClientId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    ClientId = table.Column<int>(nullable: false),
+                    StylistId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StylistClients", x => x.StylistClientId);
+                    table.ForeignKey(
+                        name: "FK_StylistClients_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "ClientId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StylistClients_Stylists_StylistId",
+                        column: x => x.StylistId,
+                        principalTable: "Stylists",
+                        principalColumn: "StylistId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StylistSpecialties",
+                columns: table => new
+                {
+                    StylistSpecialtyId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    SpecialtyId = table.Column<int>(nullable: false),
+                    StylistId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StylistSpecialties", x => x.StylistSpecialtyId);
+                    table.ForeignKey(
+                        name: "FK_StylistSpecialties_Specialties_SpecialtyId",
+                        column: x => x.SpecialtyId,
+                        principalTable: "Specialties",
+                        principalColumn: "SpecialtyId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StylistSpecialties_Stylists_StylistId",
+                        column: x => x.StylistId,
+                        principalTable: "Stylists",
+                        principalColumn: "StylistId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StylistClients_ClientId",
+                table: "StylistClients",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StylistClients_StylistId",
+                table: "StylistClients",
+                column: "StylistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StylistSpecialties_SpecialtyId",
+                table: "StylistSpecialties",
+                column: "SpecialtyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StylistSpecialties_StylistId",
+                table: "StylistSpecialties",
+                column: "StylistId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StylistClients");
+
+            migrationBuilder.DropTable(
+                name: "StylistSpecialties");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Specialties");
+
+            migrationBuilder.DropTable(
+                name: "Stylists");
+        }
+    }
+}
---
+++ HairSalon/Migrations/HairSalonContextModelSnapshot.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using HairSalon.Models;
+
+namespace HairSalon.Migrations
+{
+    [DbContext(typeof(HairSalonContext))]
+    partial class HairSalonContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1");
+
+            modelBuilder.Entity("HairSalon.Models.Client", b =>
+                {
+                    b.Property<int>("ClientId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ClientFirstName");
+
+                    b.Property<string>("ClientLastName");
+
+                    b.HasKey("ClientId");
+
+                    b.ToTable("Clients");
+                });
+
+            modelBuilder.Entity("HairSalon.Models.Specialty", b =>
+                {
+                    b.Property<int>("SpecialtyId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.HasKey("SpecialtyId");
+
+                    b.ToTable("Specialties");
+                });
+
+            modelBuilder.Entity("HairSalon.Models.Stylist", b =>
+                {
+                    b.Property<int>("StylistId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("StylistFirstName");
+
+                    b.Property<string>("StylistLastName");
+
+                    b.HasKey("StylistId");
+
+                    b.ToTable("Stylists");
+                });
+
+            modelBuilder.Entity("HairSalon.Models.StylistClient", b =>
+                {
+                    b.Property<int>("StylistClientId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("ClientId");
+
+                    b.Property<int>("StylistId");
+
+                    b.HasKey("StylistClientId");
+
+                    b.HasIndex("ClientId");
+
+                    b.HasIndex("StylistId");
+
+                    b.ToTable("StylistClients");
+                });
+
+            modelBuilder.Entity("HairSalon.Models.StylistSpecialty", b =>
+                {
+                    b.Property<int>("StylistSpecialtyId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("SpecialtyId");
+
+                    b.Property<int>("StylistId");
+
+                    b.HasKey("StylistSpecialtyId");
+
+                    b.HasIndex("SpecialtyId");
+
+                    b.HasIndex("StylistId");
+
+                    b.ToTable("StylistSpecialties");
+                });
+
+            modelBuilder.Entity("HairSalon.Models.StylistClient", b =>
+                {
+                    b.HasOne("HairSalon.Models.Client", "Client")
+                        .WithMany("StylistClients")
+                        .HasForeignKey("ClientId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("HairSalon.Models.Stylist", "Stylist")
+                        .WithMany("StylistClients")
+                        .HasForeignKey("StylistId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("HairSalon.Models.StylistSpecialty", b =>
+                {
+                    b.HasOne("HairSalon.Models.Specialty", "Specialty")
+                        .WithMany("StylistSpecialties")
+                        .HasForeignKey("SpecialtyId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("HairSalon.Models.Stylist", "Stylist")
+                        .WithMany("StylistSpecialties")
+                        .HasForeignKey("StylistId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
