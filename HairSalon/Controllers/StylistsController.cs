39bed651c360bd7e4f6ff29ce7964ad79f4984ea
Elly Maimon <eliranmaimon@gmail.com>
2018-07-20 10:57:21 -0700

    add details controller action

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace HairSalon.Controllers
 {
@@ -20,6 +21,21 @@ namespace HairSalon.Controllers
             db.Stylists.Add(stylist);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [HttpGet("/stylists/{id}")]
+        public ActionResult Details(int id)
+        {
+            Stylist author = db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+            var entryList = db.StylistClients.Where(entry => entry.StylistId == id).ToList();
+            List<Client> clientList = new List<Client>();
+            foreach (var client in entryList)
+            {
+                int clientId = client.ClientId;
+                clientList.Add(db.Clients.FirstOrDefault(record => record.ClientId == clientId));
+            }
+            ViewBag.BookList = clientList;
+            return View(author);
         }
     }
 }
